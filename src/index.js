e1d02c0f260a50fd79903e4c7cd25a39d8dae449
Vis Virial <visvirial@gmail.com>
2016-11-21 21:12:44 +0900

    simplify src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 
-var files = [
-	'Message',
-	'util',
-];
-
-module.exports = {};
-
-for(var f of files) {
-	module.exports[f] = require('./'+f+'.js');
-}
+module.exports = {
+	Message: require('./Message'),
+	util: require('./util'),
+};
 
