3f60310f21fbff51dee4ca8d4b620fd2e794b9ce
Vis Virial <visvirial@gmail.com>
2016-11-21 15:55:32 +0900

    add sample/mnemonic2address.js

---
+++ sample/mnemonic2address.js
@@ -0,0 +1,18 @@
+
+var bitcoin = require('bitcoinjs-lib');
+var xcp = require('../');
+
+if(process.argv.length < 3) {
+	console.log('usage: node mnemonic2address.js "MNEMONIC" [INDEX=0] [NETWORK=mainnet]');
+	process.exit(1);
+}
+
+var idx = 2;
+var mnemonic = process.argv[idx++];
+var index = process.argv[idx++] || 0;
+var network = process.argv[idx++] || 'mainnet';
+
+var wif = xcp.util.mnemonicToPrivateKey(mnemonic, index, network);
+console.log('WIF: ' + wif);
+console.log('address: ' + bitcoin.ECPair.fromWIF(wif, xcp.util.getBitcoinJSNetwork(network)).getAddress());
+
