61d8bc740d5c36459967869b61406747007f1d5b
Vis Virial <visvirial@gmail.com>
2016-11-23 12:34:20 +0900

    add gulp browser command

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ const pkg = require('./package');
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const stylish = require('jshint-stylish');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const del = require('del');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const babelify = require('babelify');
+const rename = require('gulp-rename');
+
+gulp.task('clean', function(cb) {
+	del(['dist'], cb);
+});
 
 gulp.task('jshint', function() {
 	return gulp
@@ -11,4 +22,24 @@ gulp.task('jshint', function() {
 		.pipe(jshint(pkg.jshintConfig))
 		.pipe(jshint.reporter(stylish));
 });
+
+gulp.task('browser:uncompressed', function() {
+	return browserify({debug: true})
+		.require('./src/index.js', {expose: 'counterjs'})
+		.transform(babelify)
+		.bundle()
+		.pipe(source('counterjs.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('browser:compressed', ['browser:uncompressed'], function(cb) {
+	pump([
+		gulp.src('./dist/counterjs.js'),
+		uglify(),
+		rename('counterjs.min.js'),
+		gulp.dest('./dist'),
+	], cb);
+});
+
+gulp.task('browser', ['browser:compressed']);
 
