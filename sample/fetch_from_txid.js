36bfa003f8a291d188950ece40988bc72c3d1b9d
Vis Virial <visvirial@gmail.com>
2016-11-24 22:42:46 +0900

    add support for network selection

--- sample/fetch_from_txid.js
+++ sample/fetch_from_txid.js
@@ -4,17 +4,22 @@
 var request = require('request');
 var xcp = require('../');
 
-const EXPLORER_TX = 'https://api.blockcypher.com/v1/btc/test3/txs/:TXID:?includeHex=true';
+const EXPLORER_TX = {
+	mainnet: 'https://api.blockcypher.com/v1/btc/main/txs/:TXID:?includeHex=true',
+	testnet: 'https://api.blockcypher.com/v1/btc/test3/txs/:TXID:?includeHex=true',
+};
 
 if(process.argv.length < 3) {
-	console.log('usage: node fetch_from_txid TXID');
+	console.log('usage: node fetch_from_txid TXID [NETWORK=mainnet]');
 	console.log('Fetch a transaction data of a given TXID from BlockCypher and parse it.');
 	process.exit(1);
 }
 
 var txid = process.argv[2];
+var network = process.argv[3] || 'mainnet';
+if(!EXPLORER_TX[network]) throw new Error('Invalid network (should be mainnet/testnet)');
 
-request(EXPLORER_TX.replace(':TXID:', txid), function(err, req, body) {
+request(EXPLORER_TX[network].replace(':TXID:', txid), function(err, req, body) {
 	if(err) throw new Error('Failed to access to the blockexplorer: ' + err.toString());
 	var json = JSON.parse(body);
 	if(json.error) throw new Error('Failed to fetch transaction data: ' + json.error);
